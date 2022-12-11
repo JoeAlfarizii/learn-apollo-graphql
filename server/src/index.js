1ef66804f96e8b78e758f11e9ab066fe92df70a7
joealfarizi <muhammad.hasan@sirclo.com>
2022-12-11 15:22:58 +0700

    liftoff-part-2 - create resolvers and datasources in server side

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const TrackAPI = require('./datasources/track-api');
 
 const mocks = {
     Query: () => ({
@@ -24,7 +27,12 @@ const mocks = {
 
 const server = new ApolloServer({
     typeDefs,
-    mocks
+    resolvers,
+    dataSources: () => {
+        return {
+            trackAPI: new TrackAPI()
+        };
+    }
 });
 server.listen().then(() => {
   console.log(`
